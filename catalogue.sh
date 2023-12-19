8f25c2b08efbcbca7b05b27f6c604c05b21600d8
gspvsr <153313380+gspvsr@users.noreply.github.com>
2023-12-18 18:19:01 -0600

    roboshop project works

---
+++ catalogue.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+
+DATE=$(date +%F)
+LOGSDIR=/tmp/
+# /home/centos/shellscript-logs/script-name-date.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
+R="\e[31m"
+N="\e[0m"
+Y="\e[33m"
+G="\e[32m"
+
+
+if [ $USERID -ne 0 ]
+then
+    echo -e "$R ERROR:: Please run script with root access $N"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then    
+        echo -e "$2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "$2 ... $G SUCCESS $N"
+    fi
+
+
+yum module disable nodejs -y &>>$LOGFILE
+VALIDATE $? "disabling nodejs"
+
+yum module enable nodejs:18 -y &>>$LOGFILE
+VALIDATE $? "enabling nodejs:18"
+
+#once the user is created, if you run this cript 2nd time
+# This command will defnitely fail
+# Improvement : first check the user already exist or not, if not exist then create
+
+useradd roboshop &>>$LOGFILE
+
+# write a condition to check directory already exist or not
+mkdir /app &>>$LOGFILE
+
+curl -o /tmp/catalogue.zip https://roboshop-builds.s3.amazonaws.com/catalogue.zip &>>$LOGFILE
+VALIDATE $? "downloading catalogue artifact"
+
+cd /app &>>$LOGFILE
+VALIDATE $? "moving to into app diectory"
+
+unzip /tmp/catalogue.zip &>>$LOGFILE
+VALIDATE $? "uzipping catalouge"
+
+npm install &>>$LOGFILE
+VALIDATE $? "installing dependencies"
+
+cp /home/centos/roboshop-shell/catalogue.service /etc/systemd/system/catalogue.service &>>$LOGFILE
+VALIDATE $? "copying catalogue.service"
+
+systemctl daemon-reload &>>$LOGFILE
+VALIDATE $? "demon-reload"
+
+systemctl enable catalogue &>>$LOGFILE
+VALIDATE $? "enabling catalogue"
+
+systemctl start catalogue &>>$LOGFILE
+VALIDATE $? "estarting catalogue"
+
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
+VALIDATE $? "copying mongo.repo"
+
+yum install mongodb-org-shell -y &>>$LOGFILE
+VALIDATE $? "installing mongo client"
+
+mongo --host mongodb.gspaws.online </app/schema/catalogue.js &>>$LOGFILE
+VALIDATE $? "catalouge data"
--- mongodb.sh
+++ mongodb.sh
@@ -19,7 +19,7 @@ then
 fi
 
 VALIDATE(){
-    if [ $? -ne 0 ];
+    if [ $1 -ne 0 ];
     then    
         echo -e "$2 ... $R FAILURE $N"
         exit 1
