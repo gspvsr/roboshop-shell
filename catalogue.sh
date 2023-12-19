8eb0c7fc804a9e67d9160eab0ba9a0745c76f387
gspvsr <153313380+gspvsr@users.noreply.github.com>
2023-12-18 19:25:48 -0600

    roboshop project works

--- catalogue.sh
+++ catalogue.sh
@@ -28,6 +28,20 @@ VALIDATE(){
     fi
 }
 
+for i in $@
+do 
+    yum list installed $i &>>$LOGFILE
+    if [ $? -ne 0 ]
+    then
+        echo "$i is not installed, let's install it"
+        yum install $i -y &>>$LOGFILE
+        VALIDATE $? "$i"
+    else
+        echo -e "$Y $i is already installed $N"
+    fi
+done
+
+
 yum module disable nodejs -y &>>$LOGFILE
 VALIDATE $? "disabling nodejs"
 
