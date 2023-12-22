e0dcea2a21db7575ec0cc2a510bd55ffc86a2a55
gspvsr <153313380+gspvsr@users.noreply.github.com>
2023-12-21 22:03:00 -0600

    new script added

--- create-ec2.sh
+++ create-ec2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NAMES=["mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web"]
+NAMES=("mongodb" "redis" "mysql" "rabbitmq" "catalogue" "user" "cart" "shipping" "payment" "dispatch" "web")
 INSTANCE_TYPE=""
 IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-03e270ec6fa525a88
@@ -9,7 +9,7 @@ SECURITY_GROUP_ID=sg-03e270ec6fa525a88
 
 for i in "${NAMES[@]}"
 do 
-    if [ [ $i == "mongodb" || $i == "mysql" ] ]
+    if [ "$i" == "mongodb" ] || [ "$i" == "mysql" ]
     then
         INSTANCE_TYPE="t3.medium"
     else
@@ -17,4 +17,4 @@ do
     fi
     echo "creating $i instance"
     aws ec2 run-instances --image-id $IMAGE_ID --instance-type "$INSTANCE_TYPE" --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
-done 
+done
