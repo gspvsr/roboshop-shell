1ade94563a2a0f7d80e5e708510eb24fb84fe0b1
gspvsr <153313380+gspvsr@users.noreply.github.com>
2023-12-21 22:00:19 -0600

    new script added

--- create-ec2.sh
+++ create-ec2.sh
@@ -9,7 +9,7 @@ SECURITY_GROUP_ID=sg-03e270ec6fa525a88
 
 for i in "${NAMES[@]}"
 do 
-    if [[ $i=="mongodb" || $i = "mysql" ]]
+    if [ [ $i == "mongodb" || $i == "mysql" ] ]
     then
         INSTANCE_TYPE="t3.medium"
     else
