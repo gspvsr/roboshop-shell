0a57b840358752e9a0ef84aa1b7c8cf6d1c7b3f7
gspvsr <153313380+gspvsr@users.noreply.github.com>
2023-12-22 19:54:42 -0600

    new script for ec2 creation

--- create-ec2.sh
+++ create-ec2.sh
@@ -6,15 +6,15 @@ IMAGE_ID=ami-03265a0778a880afb
 SECURITY_GROUP_ID=sg-03e270ec6fa525a88
 
 # if mysql or mongodb instance_type should be t3.medium, for all others it is t2.micro
-
 for i in "${NAMES[@]}"
 do 
-    if [ "$i" == "mongodb" ] || [ "$i" == "mysql" ]
+    if [[ $i == "mongodb" ] || [ $i == "mysql" ]]
     then
         INSTANCE_TYPE="t3.medium"
     else
         INSTANCE_TYPE="t2.micro"
     fi
     echo "creating $i instance"
-    aws ec2 run-instances --image-id $IMAGE_ID --instance-type "$INSTANCE_TYPE" --security-group-ids $SECURITY_GROUP_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
+    aws ec2 run-instances --image-id $IMAGE_ID --instance-type "$INSTANCE_TYPE" --security-group-ids $SECURITY_GROUP_ID 
+    --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]"
 done
